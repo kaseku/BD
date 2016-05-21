e2d5228ac0764e7fa692ff2f3bf41101bc37a6ca
kaseku <adrian.gaj@gmail.com>
2016-05-21 20:08:58 +0200

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Start</title>
+</head>
+<body>
+    <!--userID i pass: webuser , webuser -->
+    <p>Strona testowa</p>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="actionButton" Text="Dodaj element" runat="server" OnClick="onClickMetod" /> 
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    MySql.Data.MySqlClient.MySqlConnection conn;
+    MySql.Data.MySqlClient.MySqlCommand cmd;
+    String queryString;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void onClickMetod(object sender, EventArgs e)
+    {
+        addElement();
+    }
+
+    private void addElement()
+    {
+        String connString = System.Configuration.ConfigurationManager.ConnectionStrings["webAppConn"].ToString();
+
+        conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
+        conn.Open();
+        queryString = "INSERT INTO magazyn.capacitors (Name, Capacity, Passive_Id)" +
+                      "VALUES('NICHICON20','20','2')";
+
+
+        cmd = new MySql.Data.MySqlClient.MySqlCommand(queryString, conn);
+
+        cmd.ExecuteReader();
+        conn.Close();
+    }
+}
